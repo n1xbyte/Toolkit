ebc1148a3782f7fecba7162792187c34e3fc7553
Spencer <spencer@nixbyte.us>
2018-04-12 15:30:14 -0500

    Create GroupHunter.ps1

---
+++ Powershell Scripts/GroupHunter.ps1
@@ -0,0 +1,25 @@
+Function Get-Groupname {
+$Global:Groupname = Read-Host "Enter group you want to search for users logged in"
+if ($Groupname -eq $null){
+	Write-Host "Group cannot be blank"
+	Get-Groupname}
+$GroupCheck = Get-ADGroupMember $Groupname
+if ($GroupCheck -eq $null){
+	Write-Host "Invalid group"
+	Get-Groupname}
+}
+
+Get-Groupname
+
+$computers = Get-ADComputer -Filter *
+foreach ($comp in $computers)
+	{
+	$Computer = $comp.Name
+  	if (Test-Connection -ErrorAction SilentlyContinue -Count 1 -ComputerName $Computer){
+		$Proc = gwmi win32_process -ErrorAction SilentlyContinue -ComputerName $Computer -Filter "Name = 'explorer.exe'"
+		ForEach ($P in $Proc) {
+	    	$Userz = ($P.GetOwner()).User
+	    	for ($i=0; $i -le $Groupname.count; $i++){
+	  			if ([string]$Groupname[$i] -eq $Userz){
+				write-host "$Userz is logged on $Computer"
+		}}}}}
